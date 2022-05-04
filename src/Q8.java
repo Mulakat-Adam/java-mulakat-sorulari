3330c6e70018d3b1d81de8a12da1df1d73d5223f
mulakatadam <72665614+mulakatadam@users.noreply.github.com>
2022-05-04 13:36:58 +0300

    Update Q8.java

--- src/Q8.java
+++ src/Q8.java
@@ -3,7 +3,7 @@ public class Q8 {
 
 	public static boolean isPrime(int number) {
 		boolean isPrime = true;
-		if(number > 2) {
+		if(number > 1) {
 			for(int i=2;i<number;i++) {
 				if(number % i == 0) {
 					isPrime = false;
