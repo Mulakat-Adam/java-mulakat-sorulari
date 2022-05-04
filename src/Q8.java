b35c0c0833da0f8eb935a9108b2293d726fe4512
mulakatadam <72665614+mulakatadam@users.noreply.github.com>
2022-05-04 13:35:48 +0300

    Update Q8.java

--- src/Q8.java
+++ src/Q8.java
@@ -1,23 +1,28 @@
 /* bir sayının asal olup olmadıgını bulan program */
 public class Q8 {
 
-    public static boolean isPrime(int number){
-
-        boolean isPrime = true;
-        for(int i=2;i<number;i++){
-            if(number % i ==0){
-                return isPrime = false;
-            }
-        }
-        return isPrime;
-    }
+	public static boolean isPrime(int number) {
+		boolean isPrime = true;
+		if(number > 2) {
+			for(int i=2;i<number;i++) {
+				if(number % i == 0) {
+					isPrime = false;
+					break;
+				}else {
+					isPrime = true;
+				}
+			}
+		}else {
+			isPrime = false;
+		}
+		
+		return isPrime;
+	}
 
     public static void main(String[] args) {
 
-        if(isPrime(4)){
-            System.out.println("Prime");
-        }else{
-            System.out.println("NOT Prime");
-        }
+        System.out.println(isPrime(6));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(3));
     }
 }
